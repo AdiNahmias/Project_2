50343a09be453bf67c63964c9e7f457d71b3c5c9
AdiNahmias <118722490+AdiNahmias@users.noreply.github.com>
2022-11-27 21:27:45 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+#define size 10
+
+void prog_1(int mat[size][size]);
+void prog_2(int i,int j,int mat[size][size]);
+void prog_3(int j,int i,int mat[size][size]);
