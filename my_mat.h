5438b0e9febcdfef0efb429e8f52026fa448dd72
AdiNahmias <118722490+AdiNahmias@users.noreply.github.com>
2022-11-28 17:38:25 +0200

    Delete my_mat.h

--- my_mat.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-#define size 10
-
-void prog_1(int mat[size][size]);
-void prog_2(int i,int j,int mat[size][size]);
-void prog_3(int j,int i,int mat[size][size]);
-int min(int a, int b);
